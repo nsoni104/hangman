28415c2958b87c944af8da71df4d7ed6da89e26c
nsoni104 <nsoni104@gmail.com>
2019-08-03 16:43:13 -0400

    Added track word functionality, not done yet

--- hangman.py
+++ hangman.py
@@ -18,20 +18,38 @@ def enter_letter():
         else:
             print ("Please follow the directions")
 
-def track_word_progress(word,letter):
+def letter_in_word(word,letter):
     if letter in word:
+        return True
+    else:
+        return False
 
+
+def track_word(word,letter):
+    index = []
+    for char in word:
+        if letter == char:
+            print (word.index(char))
+    return index
+
+#Turn word into a list with list(word)
 
 def gameplay():
     word = start_game()
     word_length = len(word)
+    guesses = 0
     solved = False
     print("The word you are solving for has {0} letters".format(word_length))
     print("_ " * word_length)
     while solved == False:
         letter = enter_letter()
-        track_word_progress(word,letter)
+        if letter_in_word(word,letter):
+            print ("Correct!")
+        else:
+            print("Sorry, the word doesn't contain {0}.".format(letter))
+            guesses += 1
+            print("You've now had {0} guesses.".format(guesses))
 
 
 
-gameplay()
+print (track_word("apple","p"))
