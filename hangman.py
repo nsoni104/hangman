40fd29f784059730ef023687a598205a276914c7
nsoni104 <nsoni104@gmail.com>
2019-08-04 19:40:38 -0400

    Finally got enumerate working. Still need to return the right word to the user and keep track of the word separately

--- hangman.py
+++ hangman.py
@@ -26,30 +26,41 @@ def letter_in_word(word,letter):
 
 
 def track_word(word,letter):
-    index = []
-    for char in word:
-        if letter == char:
-            print (word.index(char))
-    return index
+    list_word = list(word)
+    print(list_word)
+    letter_index = []
+    for i, _ in enumerate(list_word):
+        print(i)
+        if list_word[i] == letter:
+            print(list_word[i])
+            num = i
+            letter_index.append(num)
+            #print("{0} was found!".format(letter))
+    for i in letter_index:
+        list_word[i] = letter
+    print("Your chosen letter {0} was found {1} times in the word".format(letter,len(letter_index)))
+    print(letter_index)
 
-#Turn word into a list with list(word)
+
 
 def gameplay():
     word = start_game()
     word_length = len(word)
+    letters_guessed = []
     guesses = 0
     solved = False
     print("The word you are solving for has {0} letters".format(word_length))
-    print("_ " * word_length)
+    user_word = "_ " * word_length
+    print(user_word)
     while solved == False:
         letter = enter_letter()
-        if letter_in_word(word,letter):
-            print ("Correct!")
+        if letter_in_word(word,letter) and letter not in letters_guessed:
+            print("Correct!")
+            track_word(word,letter)
         else:
-            print("Sorry, the word doesn't contain {0}.".format(letter))
             guesses += 1
+            print("Sorry, the word doesn't contain {0}.".format(letter))
             print("You've now had {0} guesses.".format(guesses))
 
 
-
-print (track_word("apple","p"))
+print(track_word("applep","p"))
